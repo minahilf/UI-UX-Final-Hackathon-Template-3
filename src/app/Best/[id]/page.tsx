016d26f44d42ace5b69e01a36cc06fa2e1c6d9d4
minahilf <sminahilfatima@gmail.com>
2024-12-27 08:04:42 -0800

    Update hackathon project for Milestone 3

--- src/app/Best/[id]/page.tsx
+++ src/app/Best/[id]/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
-import  productData2  from "../../BestAirMax/page";
 import Image from "next/image";
+import shoe1 from "../../../Assets/shoe1.png"
+import shoe2 from "../../../Assets/shoe2.png";
+import shoe3 from "../../../Assets/shoe3.png";
 import buy from "../../../../Assets/buy.png";
 import { useCart } from "@/app/Context/CartContext";
 import { useRouter } from "next/navigation";
@@ -9,6 +11,59 @@ import { use } from 'react';
 
 
 export default function ProductDetail({ params }:  { params: Promise<{ id: string }> }) {
+//  data 
+const productData2 = [
+  {
+    id:1,
+    image: shoe1,
+    name: "Nike Air Max Pulse",
+    price: "₹ 13,995",
+    category: "Womens Shoes",
+    description: "The Nike Air Max Pulse offers a stylish and comfortable fit designed for everyday wear. Featuring a sleek, modern silhouette, these shoes are equipped with a breathable mesh upper that ensures excellent ventilation, keeping your feet cool and comfortable. The iconic Air Max cushioning in the sole provides responsive support and a smooth ride with every step, making it perfect for long days on the go. With its bold design and vibrant color options, the Nike Air Max Pulse adds a touch of flair to any outfit while delivering the comfort and durability that Nike is known for. Whether you're running errands, heading to the gym, or meeting up with friends, these shoes will keep you feeling great all day long."
+  },
+  {
+    id:2,
+    image: shoe2,
+    name: "Nike Air Max Pulse",
+    price: "₹ 13,995",
+    category: "Mens Shoes",
+    description: "The Nike Air Max Pulse is a standout addition to any sneaker collection. With its cutting-edge design and performance-driven features, these shoes offer both style and comfort. The lightweight mesh upper provides breathability, ensuring your feet stay cool even during intense activities. The Air Max cushioning delivers exceptional support and cushioning, absorbing shock and enhancing comfort with each stride. Perfect for both casual wear and athletic performance, the Nike Air Max Pulse features a sleek design that pairs effortlessly with jeans, shorts, or workout gear. Whether you're training hard or enjoying a day off, these shoes will provide the comfort and style you need."
+  },
+  {
+    id:3,
+    image: shoe3,
+    name: "Nike Air Max 97 SE",
+    price: "₹ 16,995",
+    category: "Mens Shoes",
+    description: "The Nike Air Max 97 SE redefines comfort and style with its innovative design and high-performance features. Featuring the iconic full-length Air Max cushioning, these shoes offer superior comfort and support, making them perfect for long hours of wear. The premium leather and synthetic upper provides a sleek, durable look that stands out with bold color accents and reflective details. The unique, wave-inspired design of the Nike Air Max 97 SE adds a modern twist to the classic Air Max silhouette, giving you a fashion-forward look without compromising on comfort. Ideal for both casual outings and active days, the Nike Air Max 97 SE ensures you step out in style and confidence."
+  },
+  {
+    id:4,
+    image: shoe1,
+    name: "Nike Air Max Pulse",
+    price: "₹ 13,995",
+    category: "Womens Shoes",
+    description: "The Nike Air Max Pulse offers a stylish and comfortable fit designed for everyday wear. Featuring a sleek, modern silhouette, these shoes are equipped with a breathable mesh upper that ensures excellent ventilation, keeping your feet cool and comfortable. The iconic Air Max cushioning in the sole provides responsive support and a smooth ride with every step, making it perfect for long days on the go. With its bold design and vibrant color options, the Nike Air Max Pulse adds a touch of flair to any outfit while delivering the comfort and durability that Nike is known for. Whether you're running errands, heading to the gym, or meeting up with friends, these shoes will keep you feeling great all day long."
+  },
+  {
+    id:5,
+    image: shoe2,
+    name: "Nike Air Max Pulse",
+    price: "₹ 13,995",
+    category: "Mens Shoes",
+    description: "The Nike Air Max Pulse is a standout addition to any sneaker collection. With its cutting-edge design and performance-driven features, these shoes offer both style and comfort. The lightweight mesh upper provides breathability, ensuring your feet stay cool even during intense activities. The Air Max cushioning delivers exceptional support and cushioning, absorbing shock and enhancing comfort with each stride. Perfect for both casual wear and athletic performance, the Nike Air Max Pulse features a sleek design that pairs effortlessly with jeans, shorts, or workout gear. Whether you're training hard or enjoying a day off, these shoes will provide the comfort and style you need."
+  },
+  {
+    id:6,
+    image: shoe3,
+    name: "Nike Air Max 97 SE",
+    price: "₹ 16,995",
+    category: "Mens Shoes",
+    description: "The Nike Air Max 97 SE redefines comfort and style with its innovative design and high-performance features. Featuring the iconic full-length Air Max cushioning, these shoes offer superior comfort and support, making them perfect for long hours of wear. The premium leather and synthetic upper provides a sleek, durable look that stands out with bold color accents and reflective details. The unique, wave-inspired design of the Nike Air Max 97 SE adds a modern twist to the classic Air Max silhouette, giving you a fashion-forward look without compromising on comfort. Ideal for both casual outings and active days, the Nike Air Max 97 SE ensures you step out in style and confidence."
+  },
+];
+
+
   const router = useRouter();
   const { addItem } = useCart();
   const resolvedParams = use(params);
